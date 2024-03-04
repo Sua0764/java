6e960ecb83b54061e311e76e7205154ccc3bd6d1
Sua0764 <imdnasuecho@gmail.com>
2024-03-04 17:27:45 +0900

    Change

---
+++ studyJava/src/Java20240304.java
@@ -0,0 +1,55 @@
+public class Java20240304 {
+
+    public static void main(String[] args) {
+
+        int a;
+        for (a = 0; a < 3; a++) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i<3 ; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 10 ; i>0; i-- ) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i<10; i+=3) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0, j = 0; i<10; i++, j++) {
+            System.out.print( i + j + " ");
+        }
+
+        for(int i = 0;  ; i++) {
+            if(i > 10) {
+                break;
+            }
+
+        }
+        System.out.println();
+
+        for (int c = 0; c < 10; c++) {
+            System.out.print(c);
+        }
+
+        for(int a = 0; )
+
+    }
+}
+
+
+
+
+
